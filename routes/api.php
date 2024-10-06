76379b44cbc3526a73acf0caa0d8c8c1b11f380c
Navod Jayawardhana <53030244+navodjayawardhana@users.noreply.github.com>
2024-10-06 19:53:53 +0530

    Student Route Create

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,4 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //})->middleware('auth:sanctum');
 
-Route::get('/',function (){
-    return 'API';
-});
+Route::apiResource('students', StudentController::class);
